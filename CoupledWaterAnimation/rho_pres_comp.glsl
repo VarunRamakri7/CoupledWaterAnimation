78e0288df21ccb961a167efa5e757a3ac98cd748
Varun Ramakrishnan <oceans.varun@gmail.com>
2022-10-09 18:27:44 -0400

    Fixed particle radius

--- CoupledWaterAnimation/rho_pres_comp.glsl
+++ CoupledWaterAnimation/rho_pres_comp.glsl
@@ -5,7 +5,7 @@
 
 // For calculations
 #define PI 3.141592741f
-#define PARTICLE_RADIUS 0.1f
+#define PARTICLE_RADIUS 0.005f
 
 layout (local_size_x = WORK_GROUP_SIZE, local_size_y = 1, local_size_z = 1) in;
 
