eeeddd3b93b7a352648f0dfebe93d326c3dd1201
Varun Ramakrishnan <oceans.varun@gmail.com>
2022-10-10 18:57:00 -0400

    Add basic diffuse lighting

--- CoupledWaterAnimation/water-anim_fs.glsl
+++ CoupledWaterAnimation/water-anim_fs.glsl
@@ -6,15 +6,23 @@ in vec4 particle_pos;
 
 out vec4 frag_color;
 
+const vec3 particle_col = vec3(0.1f, 0.35f, 0.8f); // Blue color
+const vec3 light_col = vec3(1.0f, 0.8f, 0.65f); // Warm light_col
+const vec3 light_pos = vec3(0.0f, -0.25f, 0.0f);
+const float intensity = 10.0f;
+
 void main ()
 {
 	// Make circular particles
     float r = length(gl_PointCoord - vec2(0.35f));
     if (r >= 0.35) discard;
 
-    vec3 normal = normalize(vec3(1.0f - pow(gl_FragCoord.x, 2) - pow(gl_FragCoord.y, 2))); // Get normal of particle
+    // Calculate diffuse lighting
+    //vec3 normal = normalize(vec3(1.0f - pow(gl_FragCoord.x, 2) - pow(gl_FragCoord.y, 2))); // Get normal of particle
+    //vec3 light_dir = normalize(light_pos - particle_pos.xyz);
+    //vec3 diffuse = max(dot(normal, light_dir), 0.0f) * light_col;
+    //diffuse *= intensity;
 
-    //frag_color = vec4(particle_pos.xyz, 1.0f);
-
-    frag_color = vec4(0.1f, 0.35f, 0.8f, 1.0f);
+    //frag_color = vec4(particle_col * diffuse, 1.0f);
+    frag_color = vec4(particle_col, 1.0f);
 }
