d223417899318c6ee29028ee18bc12879aadf4a2
Ramakrishnan <ramakri7@purdue.edu>
2023-01-23 11:16:12 -0500

    Add pass uniform, and in/out blocks

--- CoupledWaterAnimation/water-anim_fs.glsl
+++ CoupledWaterAnimation/water-anim_fs.glsl
@@ -1,8 +1,16 @@
 #version 440
 
 layout(location = 1) uniform float time;
+layout(location = 2) uniform int pass;
 
-in vec4 particle_pos;
+in VertexData
+{
+	//vec4 particle_pos;
+	vec2 tex_coord;
+	vec3 pw;       //world-space vertex position
+	vec3 nw;   //world-space normal vector
+	vec3 color;
+} inData;
 
 out vec4 frag_color;
 
@@ -19,10 +27,18 @@ void main ()
 
     // Calculate diffuse lighting
     //vec3 normal = normalize(vec3(1.0f - pow(gl_FragCoord.x, 2) - pow(gl_FragCoord.y, 2))); // Get normal of particle
-    //vec3 light_dir = normalize(light_pos - particle_pos.xyz);
+    //vec3 light_dir = normalize(light_pos - inData.particle_pos.xyz);
     //vec3 diffuse = max(dot(normal, light_dir), 0.0f) * light_col;
     //diffuse *= intensity;
 
     //frag_color = vec4(particle_col * diffuse, 1.0f);
-    frag_color = vec4(particle_col, 1.0f);
+    //frag_color = vec4(particle_col, 1.0f);
+    if (pass == 0)
+    {
+        frag_color = vec4(particle_col, 1.0f);
+    }
+    else
+    {
+        frag_color = vec4(1.0f, 0.0f, 0.0f, 0.0f);
+    }
 }
--- CoupledWaterAnimation/water-anim_vs.glsl
+++ CoupledWaterAnimation/water-anim_vs.glsl
@@ -2,6 +2,7 @@
 
 layout(location = 0) uniform mat4 M;
 layout(location = 1) uniform float time;
+layout(location = 2) uniform int pass;
 
 layout(std140, binding = 0) uniform SceneUniforms
 {
@@ -9,14 +10,35 @@ layout(std140, binding = 0) uniform SceneUniforms
    vec4 eye_w;	//world-space eye position
 };
 
-in vec3 position;
+layout(location = 0) in vec3 pos_attrib; //this variable holds the position of mesh vertices
+layout(location = 1) in vec2 tex_coord_attrib;
+layout(location = 2) in vec3 normal_attrib;
+layout(location = 3) in vec3 color_attrib;
+layout(location = 4) in mat4 offset_attrib;
 
-out vec4 particle_pos;
+//out vec4 particle_pos;
+
+out VertexData
+{
+	//vec4 particle_pos;
+	vec2 tex_coord;
+	vec3 pw;       //world-space vertex position
+	vec3 nw;   //world-space normal vector
+	vec3 color;
+} outData; 
 
 void main ()
 {
-    gl_Position = PV * M * vec4(position, 1.0f);
-    particle_pos = M * vec4(position, 1.0f);
+    gl_Position = PV * M * vec4(pos_attrib, 1.0f);
+    //outData.particle_pos = M * vec4(pos_attrib, 1.0f);
+	outData.pw = vec3(M * vec4(pos_attrib, 1.0));		//world-space vertex position
+
+	if (pass == 1)
+	{
+		outData.tex_coord = tex_coord_attrib;           //send tex_coord to fragment shader
+		outData.nw = vec3(M * vec4(normal_attrib, 0.0));	//world-space normal vector
+		outData.color = color_attrib;
+	}
 
     gl_PointSize = 10.0f;
 }
