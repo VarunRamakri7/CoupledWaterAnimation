eb8d266403b9cf0bf2bcf9f5503084bd2be49fca
Ramakrishnan <ramakri7@purdue.edu>
2023-01-31 11:42:07 -0500

    Evolve wave

--- CoupledWaterAnimation/wave_comp.glsl
+++ CoupledWaterAnimation/wave_comp.glsl
@@ -5,11 +5,11 @@
 
 layout(local_size_x = 32, local_size_y = 32) in;
 
-//layout(rgba32f, binding = 0) readonly restrict uniform image2D uInputImage0; //wave at time t-1
-//layout(rgba32f, binding = 1) readonly restrict uniform image2D uInputImage1; //wave at time t-2
-//layout(rgba32f, binding = 2) writeonly restrict uniform image2D uOutputImage; //wave at time t
+layout(rgba32f, binding = 0) readonly restrict uniform image2D uInputImage0; //wave at time t-1
+layout(rgba32f, binding = 1) readonly restrict uniform image2D uInputImage1; //wave at time t-2
+layout(rgba32f, binding = 2) writeonly restrict uniform image2D uOutputImage; //wave at time t
 
-//layout (binding = 3) uniform sampler2D uInitImage;
+layout (binding = 3) uniform sampler2D uInitImage;
 
 layout(location = 1) uniform float time;
 layout(location = 3) uniform int mode;
@@ -58,9 +58,86 @@ layout(std140, binding = 3) uniform WaveUniforms
 
 const float dt = 0.0001f; // Time step
 
+void InitWave(ivec2 coord);
+void InitFromImage(ivec2 coord);
+void EvolveWave(ivec2 coord, ivec2 size);
+
+struct neighborhood
+{
+	vec4 c1;
+	vec4 c0;
+	vec4 n0;
+	vec4 s0;
+	vec4 e0;
+	vec4 w0;
+};
+
+neighborhood get_clamp(ivec2 coord);
+
 void main()
 {
 	ivec2 gid = ivec2(gl_GlobalInvocationID.xy);
+	ivec2 size = imageSize(uOutputImage);
 
-    waves[gid.x].pos = vec4(Lambda, Atten, Beta, 0.0f);
+	if(any(greaterThanEqual(gid.xy, size))) return;
+
+	switch(mode)
+	{
+		case MODE_INIT:
+			InitWave(gid);
+		break;
+
+		case MODE_INIT_FROM_TEXTURE:
+			InitFromImage(gid);
+		break;
+
+		case MODE_EVOLVE:
+			EvolveWave(gid, size);
+		break;
+	}
+}
+
+void InitFromImage(ivec2 coord)
+{
+	vec4 vout = texelFetch(uInitImage, coord, 0);
+	imageStore(uOutputImage, coord, vout);
+}
+
+void InitWave(ivec2 coord)
+{
+	vec4 vout = vec4(0.0);
+	ivec2 size = imageSize(uOutputImage);
+	ivec2 cen0 = ivec2(0.25*size);
+	ivec2 cen1 = ivec2(0.75*size);
+
+	float d = min(distance(coord, cen0), distance(coord, cen1));
+	vout.x = 0.5*smoothstep(3.0, 0.0, d);
+	imageStore(uOutputImage, coord, vout);
+}
+
+void EvolveWave(ivec2 coord, ivec2 size)
+{
+	neighborhood n = get_clamp(coord);
+	vec4 w = (2.0 - 4.0 * Lambda - Beta) * n.c0 + Lambda * (n.n0 + n.s0 + n.e0 + n.w0) - (1.0 - Beta) * n.c1;
+	w = Atten*w;
+    imageStore(uOutputImage, coord, w);
+}
+
+ivec2 clamp_coord(ivec2 coord)
+{
+   ivec2 size = imageSize(uOutputImage);
+   return ivec2(clamp(coord, ivec2(0), size-ivec2(1)));
+}
+
+neighborhood get_clamp(ivec2 coord)
+{
+	neighborhood n;
+	n.c1 = imageLoad(uInputImage1, coord);
+	n.c0 = imageLoad(uInputImage0, coord);
+	n.n0 = imageLoad(uInputImage0, clamp_coord(coord+ivec2(0,+1)));
+	n.s0 = imageLoad(uInputImage0, clamp_coord(coord+ivec2(0,-1)));
+	n.e0 = imageLoad(uInputImage0, clamp_coord(coord+ivec2(+1,0)));
+	n.w0 = imageLoad(uInputImage0, clamp_coord(coord+ivec2(-1,0)));
+
+   return n;
 }
