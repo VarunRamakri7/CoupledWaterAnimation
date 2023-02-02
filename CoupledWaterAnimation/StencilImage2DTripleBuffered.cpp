6ee7fdd6b8e68ea8972556c759fcd81c44a7e6fa
Varun Ramakrishnan <ramakri7@purdue.edu>
2023-02-02 11:17:17 -0500

    Working wave surface

--- CoupledWaterAnimation/StencilImage2DTripleBuffered.cpp
+++ CoupledWaterAnimation/StencilImage2DTripleBuffered.cpp
@@ -81,8 +81,8 @@ void StencilImage2DTripleBuffered::Compute()
    if (mEvolve == false) return;
    if (pShader == nullptr) return;
 
-   //pShader->SetMode(MODE_EVOLVE); // Set mode to evolve
-   pShader->SetMode(MODE_TEST); // Set mode to test
+   pShader->SetMode(MODE_EVOLVE); // Set mode to evolve
+   //pShader->SetMode(MODE_TEST); // Set mode to test
 
    mImage[mReadIndex[0]].BindImageTexture(GL_READ_ONLY);
    mImage[mReadIndex[1]].BindImageTexture(GL_READ_ONLY);
--- CoupledWaterAnimation/wave_vs.glsl
+++ CoupledWaterAnimation/wave_vs.glsl
@@ -9,6 +9,8 @@ layout(std140, binding = 0) uniform SceneUniforms
    vec4 eye_w;	//world-space eye position
 };
 
+layout(binding = 0) uniform sampler2D diffuse_tex;
+
 layout(location = 0) in vec4 pos_attrib; //this variable holds the position of mesh vertices
 layout(location = 1) in vec4 tex_coord_attrib;
 layout(location = 2) in vec4 normal_attrib;
@@ -25,8 +27,13 @@ out VertexData
 
 void main(void)
 {
-	gl_Position = PV * M * offset_attrib * vec4(pos_attrib.xyz, 1.0); //transform vertices and send result into pipeline
-	
+	vec4 pos = vec4(pos_attrib.xyz, 1.0);
+
+	float height = textureLod(diffuse_tex, tex_coord_attrib.xy, 0.0).r;
+	pos.y = 4.0 * height;
+
+	gl_Position = PV * M * offset_attrib * pos; //transform vertices and send result into pipeline
+
 	//Use dot notation to access members of the interface block
 	outData.tex_coord = tex_coord_attrib.xy;           //send tex_coord to fragment shader
 	outData.pw = vec3(M * vec4(pos_attrib.xyz, 1.0));		//world-space vertex position
