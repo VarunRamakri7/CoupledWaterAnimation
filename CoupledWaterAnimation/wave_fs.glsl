1b6665617324f2490c4953aca64014f41d3d67ba
Varun Ramakrishnan <ramakri7@purdue.edu>
2023-02-02 11:30:53 -0500

    Update wave surface color depending on height

--- CoupledWaterAnimation/wave_fs.glsl
+++ CoupledWaterAnimation/wave_fs.glsl
@@ -21,7 +21,15 @@ void main(void)
 	//fragcolor = vec4(inData.tex_coord, 0.0, 1.0); //debug view tex coords
 	//fragcolor = vec4(abs(nw), 1.0); //debug view normals
 
-	fragcolor = vec4(wave_col, 1.0f);
+	//fragcolor = vec4(wave_col, 1.0f);
+
+	vec4 color0 = vec4(1.0f, 0.95f, 0.85, 1.0f); // Whitish-Red
+	vec4 color1 = vec4(0.9f, 0.6f, 0.0f, 1.0f); // Purple
+	vec4 v = texture(diffuse_tex, inData.tex_coord);
+	v.x = smoothstep(-0.01, 0.01, v.x);
+	
+	fragcolor = vec4(mix(color1, color0, v.x));
+
 }
 
 /*in vec2 tex_coord; 
