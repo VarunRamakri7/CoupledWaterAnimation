6ca08df472ab694eac64e86081505d907f30aadb
Varun Ramakrishnan <ramakri7@purdue.edu>
2023-02-02 11:31:38 -0500

    Removed unused code comments

--- CoupledWaterAnimation/wave_fs.glsl
+++ CoupledWaterAnimation/wave_fs.glsl
@@ -31,18 +31,3 @@ void main(void)
 	fragcolor = vec4(mix(color1, color0, v.x));
 
 }
-
-/*in vec2 tex_coord; 
-
-out vec4 fragcolor; //the output color for this fragment    
-
-void main(void)
-{   
-	vec4 color0 = vec4(1.0f, 0.95f, 0.85, 1.0f); // Whitish-Red
-	vec4 color1 = vec4(0.9f, 0.6f, 0.0f, 1.0f); // Purple
-	vec4 v = texture(diffuse_tex, tex_coord);
-	v.x = smoothstep(-0.01, 0.01, v.x);
-	
-	fragcolor = vec4(mix(color1, color0, v.x));
-	//fragcolor = vec4(1.0);
-}*/
