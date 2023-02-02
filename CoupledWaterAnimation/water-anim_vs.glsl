c4f81bcf602a22eab053f1603bb95cd5d8d25e69
Varun Ramakrishnan <ramakri7@purdue.edu>
2023-02-02 17:14:49 -0500

    Added wave texture

--- CoupledWaterAnimation/water-anim_vs.glsl
+++ CoupledWaterAnimation/water-anim_vs.glsl
@@ -6,16 +6,27 @@ layout(location = 2) uniform int pass;
 
 layout(std140, binding = 0) uniform SceneUniforms
 {
-   mat4 PV;	//camera projection * view matrix
-   vec4 eye_w;	//world-space eye position
+   mat4 PV;
+   vec4 eye_w; // Camera eye in world-space
 };
 
-layout(location = 0) in vec3 pos_attrib; //this variable holds the position of mesh vertices
+layout(binding = 0) uniform sampler2D diffuse_tex;
+
+layout(location = 0) in vec3 pos_attrib;
+layout(location = 1) in vec4 tex_coord_attrib;
 
 out vec3 particle_pos;
 
 void main ()
 {
+	//vec4 pos = vec4(pos_attrib.xyz, 1.0);
+
+	//float height = textureLod(diffuse_tex, tex_coord_attrib.xy, 0.0).r;
+	//pos.y = 50.0f * height;
+
+	//gl_Position = PV * M * pos;
+	//particle_pos = vec3(M * pos);
+
     gl_Position = PV * M * vec4(pos_attrib, 1.0f);
 	particle_pos = vec3(M * vec4(pos_attrib, 1.0));
 }
