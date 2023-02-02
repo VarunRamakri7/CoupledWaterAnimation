72a3bb6e893c78de6f9bcdf3b2abb72d70aed6f3
Varun Ramakrishnan <ramakri7@purdue.edu>
2023-02-02 17:04:02 -0500

    Removed extra comments

--- CoupledWaterAnimation/wave_fs.glsl
+++ CoupledWaterAnimation/wave_fs.glsl
@@ -6,14 +6,16 @@ layout(location = 1) uniform float time;
 in VertexData
 {
    vec2 tex_coord;
-   vec3 pw;       //world-space vertex position
-   vec3 nw;   //world-space normal vector
+   vec3 pw;
+   vec3 nw;
    vec3 color;
 } inData;
 
-out vec4 fragcolor; //the output color for this fragment  
+out vec4 fragcolor;
 
-const vec3 wave_col = vec3(1.0f, 0.0f, 0.0f); // Red color
+const vec3 wave_col = vec3(1.0f, 0.0f, 0.0f); // Red
+const vec4 color0 = vec4(1.0f, 0.95f, 0.85, 1.0f); // Whitish-Red
+const vec4 color1 = vec4(0.9f, 0.6f, 0.0f, 1.0f); // Purple
 
 void main(void)
 {   
@@ -23,8 +25,6 @@ void main(void)
 
 	//fragcolor = vec4(wave_col, 1.0f);
 
-	vec4 color0 = vec4(1.0f, 0.95f, 0.85, 1.0f); // Whitish-Red
-	vec4 color1 = vec4(0.9f, 0.6f, 0.0f, 1.0f); // Purple
 	vec4 v = texture(diffuse_tex, inData.tex_coord);
 	v.x = smoothstep(-0.01, 0.01, v.x);
 	
