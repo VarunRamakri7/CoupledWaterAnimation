3a2a7f84e9b3c08b0397afa5b9c495d471bc65d8
Varun Ramakrishnan <ramakri7@purdue.edu>
2023-02-07 17:31:19 -0500

    Swizzle Y and Z components for wave velocity

--- CoupledWaterAnimation/force_comp.glsl
+++ CoupledWaterAnimation/force_comp.glsl
@@ -63,7 +63,7 @@ void main()
 	ivec2 coord = ivec2(particles[i].pos.xz) / texture_size; // Get XZ coordinate of particle
 	wave_particle.pos = particles[i].pos; // Set the same position as the current particle
 	wave_particle.pos.y -= 2.0f * PARTICLE_RADIUS; // Set ghost particle height jsut below surface
-	wave_particle.vel = vec4(WaveVelocity(coord), 0.0f); // Calculate velocity of the wave at this point
+	wave_particle.vel = vec4(WaveVelocity(coord).xzy, 0.0f); // Calculate velocity of the wave at this point
 	
 	vec3 wave_acc = (wave_particle.vel.xyz - wave_particle.vel.xyz) / dt;
 	
@@ -113,7 +113,7 @@ vec3 WaveVelocity(ivec2 uv)
     float height = texture(wave_tex, uv).r;
     float heightX = texture(wave_tex, vec2(uv.x + h, uv.y)).r;
     float heightY = texture(wave_tex, vec2(uv.x, uv.y + h)).r;
-    float heightZ = texture(wave_tex, vec2(uv.x, uv.y)).r;
+    float heightZ = texture(wave_tex, uv).r;
     float heightZX = texture(wave_tex, vec2(uv.x + h, uv.y)).r;
     float heightZY = texture(wave_tex, vec2(uv.x, uv.y + h)).r;
 
