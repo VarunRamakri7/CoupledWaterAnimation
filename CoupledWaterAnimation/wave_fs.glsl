537d69a520ebe98346f51ee3e5683c8413899add
Varun Ramakrishnan <ramakri7@purdue.edu>
2023-02-08 11:50:06 -0500

    Removed unused code

--- CoupledWaterAnimation/wave_fs.glsl
+++ CoupledWaterAnimation/wave_fs.glsl
@@ -19,15 +19,8 @@ const vec4 color1 = vec4(0.9f, 0.6f, 0.0f, 1.0f); // Purple
 
 void main(void)
 {   
-	//fragcolor = vec4(1.0, 0.0, 0.0, 1.0);
-	//fragcolor = vec4(inData.tex_coord, 0.0, 1.0); //debug view tex coords
-	//fragcolor = vec4(abs(nw), 1.0); //debug view normals
-
-	//fragcolor = vec4(wave_col, 1.0f);
-
 	vec4 v = texture(wave_tex, inData.tex_coord);
 	v.x = smoothstep(-0.01, 0.01, v.x);
 	
 	fragcolor = vec4(mix(color1, color0, v.x));
-
 }
