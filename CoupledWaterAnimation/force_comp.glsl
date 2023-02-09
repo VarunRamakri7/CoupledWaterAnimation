1ab1aabc865dc43a863e6bbb957f14b0ad9c86d0
Varun Ramakrishnan <ramakri7@purdue.edu>
2023-02-09 14:54:33 -0500

    Updated timestep

--- CoupledWaterAnimation/force_comp.glsl
+++ CoupledWaterAnimation/force_comp.glsl
@@ -54,7 +54,7 @@ layout(std140, binding = 2) uniform BoundaryUniform
 
 const vec3 G = vec3(0.0f, -9806.65f, 0.0f); // Gravity force
 const ivec2 texture_size = textureSize(wave_tex, 0);
-const float dt = 0.00008f; // Time step
+const float dt = 0.001f; // Time step
 
 Particle wave_particle;
 
@@ -81,7 +81,7 @@ void main()
 		}
 
 		vec3 delta = particles[i].pos.xyz - particles[j].pos.xyz; // Get vector between current particle and particle in vicinity
-		float r = length(delta); // Get length of ramthe vector
+		float r = length(delta); // Get length of the vector
 		if (r < smoothing_length) // Check if particle is inside smoothing radius
 		{
 			pres_force -= mass * (particles[i].extras[1] + particles[j].extras[1]) / (2.0f * particles[j].extras[0]) * spiky * pow(smoothing_length - r, 2) * normalize(delta); // Gradient of Spiky Kernel
@@ -92,14 +92,14 @@ void main()
 	// Make wave particle
 	ivec2 coord = ivec2(particles[i].pos.xz) / texture_size; // Get XZ coordinate of particle
 	wave_particle.pos = particles[i].pos; // Set the same position as the current particle
-	wave_particle.pos.y = lower.y - (PARTICLE_RADIUS + PARTICLE_RADIUS); // Set height of particle just below the wave surface
+	wave_particle.pos.y = lower.y - (2.0f * PARTICLE_RADIUS); // Set height of particle just below the wave surface
 	wave_particle.vel = vec4(WaveVelocity(coord).xzy, 0.0f); // Calculate velocity of the wave at this point
-
+	
 	vec3 wave_acc = (wave_particle.vel.xyz - particles[i].vel.xyz) / dt;
-
+	
 	wave_particle.force = vec4(mass * wave_acc, 0.0f); // Force exerted by wave
-	wave_particle.extras = vec4(100.0f * resting_rho, 0.00000178f, 0.0f, 0.0f); // Density, pressure, and age
-
+	wave_particle.extras = vec4(100.0f * resting_rho, particles[i].extras[1], 0.0f, 0.0f); // Density, pressure, and age
+	
 	// Add force from ghost wave particle
 	vec3 wave_delta = particles[i].pos.xyz - wave_particle.pos.xyz; // Vector between wave ghost particle and current particle
 	float wave_r = max(0.0f, length(wave_delta));
@@ -117,9 +117,10 @@ void main()
 
 vec3 WaveVelocity(ivec2 uv)
 {
-    float h = 0.01; // Small step
+    float h = 0.01f; // Small step
 
     float height = texture(wave_tex, uv).r;
+
     float heightX = texture(wave_tex, vec2(uv.x + h, uv.y)).r;
     float heightY = texture(wave_tex, vec2(uv.x, uv.y + h)).r;
     float heightZ = texture(wave_tex, uv).r;
