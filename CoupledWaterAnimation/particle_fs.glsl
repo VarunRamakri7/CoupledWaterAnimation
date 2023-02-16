6a39de4af548a6dbfb352bb4e946cd3ba72c2a63
Varun Ramakrishnan <ramakri7@purdue.edu>
2023-02-16 16:16:46 -0500

    Misc. changes to color

--- CoupledWaterAnimation/particle_fs.glsl
+++ CoupledWaterAnimation/particle_fs.glsl
@@ -11,12 +11,12 @@ in vec2 tex_coord;
 out vec4 frag_color;
 
 const vec4 particle_col = vec4(0.1f, 0.65f, 0.8f, 1.0f); // Blue color
-const vec4 color0 = vec4(0.85f, 0.95f, 1.00, 1.0f); // Whitish-Blue
+const vec4 color0 = vec4(0.75f, 0.75f, 1.0f, 1.0f); // Whitish-Blue
 const vec4 color1 = vec4(0.9f, 0.6f, 0.0f, 1.0f); // Purple
 
 void main ()
 {
-	if (particle_pos.y < 0.0f) discard; // Ignore particles that are below surface
+	//if (particle_pos.y < 0.0f) discard; // Ignore particles that are below surface
 
 	// Make circular particles
     float r = length(gl_PointCoord - vec2(0.35f));
@@ -24,7 +24,7 @@ void main ()
 
     frag_color = particle_col;
 
-	//vec4 v = texture(wave_tex, tex_coord);
+	//vec4 v = texture(wave_tex, particle_pos.xz);
 	//v.x = smoothstep(-0.01, 0.01, v.x);
 	
 	//frag_color = vec4(mix(particle_col, color0, v.x));
