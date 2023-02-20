823774f0f38b87fc3492adbfc0a4ef4df5909732
Varun Ramakrishnan <ramakri7@purdue.edu>
2023-02-20 16:38:42 -0500

    Reflect skybox

--- CoupledWaterAnimation/wave_fs.glsl
+++ CoupledWaterAnimation/wave_fs.glsl
@@ -4,6 +4,12 @@ layout(binding = 0) uniform sampler2D wave_tex;
 layout(binding = 1) uniform samplerCube skybox_tex;
 layout(location = 1) uniform float time;
 
+layout(std140, binding = 0) uniform SceneUniforms
+{
+   mat4 PV;
+   vec4 eye_w; // Camera eye in world-space
+};
+
 in VertexData
 {
    vec2 tex_coord;
@@ -17,12 +23,21 @@ out vec4 fragcolor;
 const vec4 wave_col = vec4(0.0f, 0.5f, 1.0f, 1.0f); // Azure
 const vec4 color0 = vec4(0.6f, 0.8f, 1.0f, 1.0f); // Baby-blue
 
+vec4 reflection();
+
 void main(void)
 {
-	vec4 v = texture(wave_tex, inData.tex_coord);
-	v.x = smoothstep(-0.01, 0.01, v.x);
+	//vec4 v = texture(wave_tex, inData.tex_coord);
+	//v.x = smoothstep(-0.01, 0.01, v.x);
 	
-	fragcolor = vec4(mix(wave_col, color0, v.x));
+	//fragcolor = vec4(mix(wave_col, color0, v.x));
 
-	//fragcolor = texture(skybox_tex, normalize(inData.pw));
+	fragcolor = reflection();
+}
+
+vec4 reflection()
+{
+    vec3 I = normalize(inData.pw - eye_w.xyz);
+    vec3 R = reflect(I, normalize(inData.nw));
+    return vec4(texture(skybox_tex, R).rgb, 1.0f);
 }
