33f79aef5895e1d6f10943c57b965bd2f9990782
Varun Ramakrishnan <ramakri7@purdue.edu>
2023-02-20 13:46:04 -0500

    Add null check
     - Delete array

--- CoupledWaterAnimation/LoadTexture.cpp
+++ CoupledWaterAnimation/LoadTexture.cpp
@@ -45,17 +45,22 @@ GLuint LoadCubemap(const std::vector<std::string>& faces)
         FIBITMAP* tempImg = FreeImage_Load(FreeImage_GetFileType(faces[i].c_str(), 0), faces[i].c_str());
         FIBITMAP* img = FreeImage_ConvertTo32Bits(tempImg);
 
-        FreeImage_Unload(tempImg);
+        if (tempImg != nullptr && img != nullptr)
+        {
+            FreeImage_Unload(tempImg);
 
-        GLuint w = FreeImage_GetWidth(img);
-        GLuint h = FreeImage_GetHeight(img);
-        GLuint scanW = FreeImage_GetPitch(img);
+            GLuint w = FreeImage_GetWidth(img);
+            GLuint h = FreeImage_GetHeight(img);
+            GLuint scanW = FreeImage_GetPitch(img);
 
-        byteImg = new GLubyte[h * scanW];
-        FreeImage_ConvertToRawBits(byteImg, img, scanW, 32, FI_RGBA_RED_MASK, FI_RGBA_GREEN_MASK, FI_RGBA_BLUE_MASK, FALSE);
-        FreeImage_Unload(img);
+            byteImg = new GLubyte[h * scanW];
+            FreeImage_ConvertToRawBits(byteImg, img, scanW, 32, FI_RGBA_RED_MASK, FI_RGBA_GREEN_MASK, FI_RGBA_BLUE_MASK, FALSE);
+            FreeImage_Unload(img);
 
-        glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, byteImg);
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, byteImg);
+
+            delete[] byteImg;
+        }
     }
 
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
@@ -63,8 +68,6 @@ GLuint LoadCubemap(const std::vector<std::string>& faces)
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-
-    delete byteImg;
 
     return tex_id;
 }
