52401b15b1da9b68437cac9d1fbea5e89af340eb
Varun Ramakrishnan <ramakri7@purdue.edu>
2023-02-20 17:13:37 -0500

    Added basic lighting

--- CoupledWaterAnimation/wave_fs.glsl
+++ CoupledWaterAnimation/wave_fs.glsl
@@ -23,17 +23,31 @@ out vec4 fragcolor;
 const vec4 wave_col = vec4(0.0f, 0.5f, 1.0f, 1.0f); // Azure
 const vec4 color0 = vec4(0.6f, 0.8f, 1.0f, 1.0f); // Baby-blue
 
+const vec3 light_col = vec3(1.0f, 0.85f, 0.7f); // Warm light
+const vec3 light_pos = vec3(0.0f, 1.0f, 0.0f); // Light position
+const vec4 base_col = vec4(0.5f, 0.92f, 1.0f, 1.0f);
+
 vec4 reflection();
 vec4 refraction();
+vec4 lighting();
 
 void main(void)
 {
 	vec4 v = texture(wave_tex, inData.tex_coord);
 	v.x = smoothstep(-0.01, 0.01, v.x);
 	
-	//fragcolor = vec4(mix(wave_col, color0, v.x));
+	//fragcolor = vec4(mix(wave_col, color0, v.x)); // Color wave depending on height
 
-	fragcolor = mix(refraction(), color0, v.x);
+    //fragcolor = 0.5f * (refraction() + reflection()); // Color wave with refraction and reflection
+    //fragcolor = 5.0f * lighting();// Color wave with lighting
+
+    vec4 refraction_color = refraction();
+    vec4 reflection_color = reflection();
+    vec4 lighting_color = lighting();
+
+    vec4 final_color = mix(0.5f * (refraction_color + reflection_color) + lighting_color, base_col, v.x);
+
+    fragcolor = final_color;
 }
 
 vec4 reflection()
@@ -49,4 +63,26 @@ vec4 refraction()
     vec3 I = normalize(inData.pw - eye_w.xyz);
     vec3 R = refract(I, normalize(inData.nw), ratio);
     return vec4(texture(skybox_tex, R).rgb, 1.0f);
+}
+
+vec4 lighting()
+{
+    // ambient
+    float ambientStrength = 0.2f;
+    vec3 ambient = ambientStrength * light_col;
+  	
+    // diffuse 
+    vec3 norm = normalize(inData.nw);
+    vec3 lightDir = normalize(light_pos - inData.pw);
+    float diff = max(dot(norm, lightDir), 0.0f);
+    vec3 diffuse = diff * light_col;
+    
+    // specular
+    float specularStrength = 0.8f;
+    vec3 viewDir = normalize(eye_w.xyz - inData.pw);
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0f), 32);
+    vec3 specular = specularStrength * spec * light_col;  
+        
+    return vec4((ambient + diffuse + specular) * color0.xyz, 1.0f);
 }
