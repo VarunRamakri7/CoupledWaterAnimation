0b1b13b2e6b1382d97d0ab8b42a6da1e9111f7a7
Varun Ramakrishnan <ramakri7@purdue.edu>
2023-02-20 18:02:54 -0500

    Add slight specular factor to particle color

--- CoupledWaterAnimation/particle_fs.glsl
+++ CoupledWaterAnimation/particle_fs.glsl
@@ -10,18 +10,25 @@ in vec2 tex_coord;
 
 out vec4 frag_color;
 
-const vec4 particle_col = vec4(0.1f, 0.7f, 1.0f, 1.0f); // Blue
-const vec4 color0 = vec4(0.7f, 0.85f, 1.0f, 1.0f); // Whitish-Blue
+//const vec4 particle_col = vec4(0.8f, 0.97f, 1.0f, 1.0f); // Light cyan
+const vec4 particle_col = vec4(0.2f, 0.5f, 1.0f, 1.0f); // Light blue
+const vec4 foam_col = vec4(1.0f); // White
 
 void main ()
 {
-	//if (particle_pos.y < 0.0f) discard; // Ignore particles that are below surface
+	if (particle_pos.y < 0.0f) discard; // Ignore particles that are below surface
 
 	// Make circular particles
-    float r = length(gl_PointCoord - vec2(0.35f));
-    if (r >= 0.35) discard;
-
-    //frag_color = particle_col;
+    float r = length(gl_PointCoord - vec2(0.45f));
+    if (r >= 0.45f) discard;
 	
-	frag_color = vec4(mix(particle_col, color0, 2.0f * particle_pos.y)); // Change particle color depending on height
+	//frag_color = vec4(mix(particle_col, foam_col, 2.0f * particle_pos.y)); // Change particle color depending on height
+
+	// Add specular highlight
+	float spec = pow(max(dot(normalize(particle_pos), normalize(vec3(0.0f, 1.0f, 0.0f))), 0.0f), 16.0f);
+
+	// Change particle color depending on height
+	frag_color = mix(particle_col + vec4(0.2f, 0.2f, 0.2f, 0.0f), foam_col, 2.0f * particle_pos.y);
+	frag_color.a = 1.0f;
+	frag_color.rgb += spec; // Add specular highlight
 }
