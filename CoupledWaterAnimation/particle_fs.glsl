ab1e8cae0b76542863e6f4aad37b184466d37a0b
Varun Ramakrishnan <ramakri7@purdue.edu>
2023-02-22 12:10:47 -0500

    Get texture height if needed

--- CoupledWaterAnimation/particle_fs.glsl
+++ CoupledWaterAnimation/particle_fs.glsl
@@ -16,14 +16,13 @@ const vec4 foam_col = vec4(1.0f); // White
 
 void main ()
 {
-	if (particle_pos.y < 0.0f) discard; // Ignore particles that are below surface
+	//float height = textureLod(wave_tex, particle_pos.xz * 2.0f, 0.0f).r;
+	if(particle_pos.y < 0.0f) discard; // Ignore particles that are below origin
 
 	// Make circular particles
     float r = length(gl_PointCoord - vec2(0.4f));
     if (r >= 0.4f) discard;
 	
-	//frag_color = vec4(mix(particle_col, foam_col, 2.0f * particle_pos.y)); // Change particle color depending on height
-
 	// Add specular highlight
 	float spec = pow(max(dot(normalize(particle_pos), normalize(vec3(0.0f, 1.0f, 0.0f))), 0.0f), 16.0f);
 
