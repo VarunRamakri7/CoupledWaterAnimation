a8a02f8ec574b93cd19a8a1b00807f8da8f26ed5
Ramakrishnan <ramakri7@purdue.edu>
2023-03-06 12:20:49 -0500

    Render particle like a glass sphere

--- CoupledWaterAnimation/particle_fs.glsl
+++ CoupledWaterAnimation/particle_fs.glsl
@@ -10,8 +10,10 @@ in vec2 tex_coord;
 
 out vec4 frag_color;
 
-const vec4 particle_col = vec4(0.2f, 0.5f, 1.0f, 1.0f); // Light blue
+const vec4 particle_col = vec4(0.4f, 0.7f, 1.0f, 1.0f); // Light blue
 const vec4 foam_col = vec4(1.0f); // White
+
+float rand(vec2 co);
 
 void main ()
 {
@@ -19,14 +21,21 @@ void main ()
 	//if (particle_pos.y < 0.0f) discard; // Ignore particles that are below origin
 
 	// Make circular particles
-    float r = length(gl_PointCoord - vec2(0.4f));
-    if (r >= 0.4f) discard;
+    float r = length(gl_PointCoord - vec2(0.5f));
+    if (r >= 0.5f) discard;
 	
 	// Add specular highlight
 	float spec = pow(max(dot(normalize(particle_pos), normalize(vec3(0.0f, 1.0f, 0.0f))), 0.0f), 16.0f);
+	float a = exp(-12.0f * r) + 0.1f * exp(-2.0f * r);
+	a *= rand(particle_pos.xz);
 
 	// Change particle color depending on height
-	frag_color = mix(particle_col + vec4(0.2f, 0.2f, 0.2f, 0.0f), foam_col, 2.0f * particle_pos.y);
+	frag_color = mix(particle_col, foam_col, 2.0f * particle_pos.y);
 	frag_color.rgb += spec; // Add specular highlight
-	frag_color.a = 0.5f;
+	frag_color.a = mix(1.0f, a, 2.0f * r); // Change transparency based on distance from center
+}
+
+float rand(vec2 co)
+{
+    return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
 }
